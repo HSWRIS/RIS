d31b9c9f1d8b3677c01f8cd6aeb2ef9ff16d7ba5
hswcdl <chendaliang@hsware.cn>
2016-02-03 11:11:48 +0800

    111

---
+++ HSWRIS/RISClient/Shujuku/Shujuku.cs
@@ -0,0 +1,20 @@
+﻿namespace RISClient.Shujuku
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class Shujuku : DbContext
+    {
+        public Shujuku()
+            : base("name=Shujuku")
+        {
+        }
+
+      
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+  
+}
--- HSWRIS/RISClient/Yemian/Xitong/Denglu.xaml.cs
+++ HSWRIS/RISClient/Yemian/Xitong/Denglu.xaml.cs
@@ -10,17 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
-namespace RISClient
+namespace RISClient.Yemian.Xitong
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// Denglu.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Denglu : MetroWindow
     {
-        public MainWindow()
+        public Denglu()
         {
             InitializeComponent();
         }
